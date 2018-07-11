ce53620f4620c92c764c03ede52086a2c6defa62
Liruilos <lolliewalton@gmail.com>
2018-07-11 15:47:44 +0200

    moved crate set over from sundered blocks

--- src/main/java/net/grallarius/sundereddeco/block/ModBlocks.java
+++ src/main/java/net/grallarius/sundereddeco/block/ModBlocks.java
@@ -7,38 +7,66 @@ import net.grallarius.sundereddeco.block.garden.flowerbeds.BlockFlowerbed;
 import net.grallarius.sundereddeco.block.garden.BlockFountain;
 import net.grallarius.sundereddeco.block.garden.BlockPlanterbox;
 import net.grallarius.sundereddeco.block.garden.windowbox.BlockWindowbox;
+import net.grallarius.sundereddeco.block.shop.BlockCrate;
 
 public class ModBlocks {
 
     public static BlockOre oreCopper = new BlockOre("ore_copper", "oreCopper");
-    public static BlockFountain fountain = new BlockFountain("fountain");
-    public static BlockPlanterbox largeplanterbox = new BlockPlanterbox("largeplanterbox");
-    public static BlockWindowbox windowbox = new BlockWindowbox("windowbox");
+
     public static BlockTable table = new BlockTable();
     public static BlockSittable stool = new BlockSittable("stool");
     public static BlockChair chair = new BlockChair("chair");
     public static BlockConnectedChair parkbench = new BlockConnectedChair();
 
+
+    /** garden blocks */
+    public static BlockFountain fountain = new BlockFountain("fountain");
+    public static BlockPlanterbox largeplanterbox = new BlockPlanterbox("largeplanterbox");
+    public static BlockWindowbox windowbox = new BlockWindowbox("windowbox");
     public static BlockFlowerbed flowerbed = new BlockFlowerbed("flowerbed");
     public static BlockDenseFlowerbed denseFlowerbed = new BlockDenseFlowerbed("denseflowerbed");
+
+    /** shop blocks */
+    public static BlockCrate crateClosed = new BlockCrate("crate_closed");
+    public static BlockCrate crateApple = new BlockCrate("crate_apple");
+    public static BlockCrate crateBerry = new BlockCrate("crate_berry");
+    public static BlockCrate crateCarrot = new BlockCrate("crate_carrot");
+    public static BlockCrate crateFish = new BlockCrate("crate_fish");
+    public static BlockCrate crateGrain = new BlockCrate("crate_grain");
+    public static BlockCrate crateIngot = new BlockCrate("crate_ingot");
+    public static BlockCrate cratePotato = new BlockCrate("crate_potato");
+    public static BlockCrate crateSugar = new BlockCrate("crate_sugar");
 
 
     /*    public static BlockPedestal pedestal = new BlockPedestal();*/
 
     public static void preInit() {
                 oreCopper.register();
-                fountain.register();
-                largeplanterbox.register();
-                windowbox.register();
+
                 table.register();
                 stool.register();
                 chair.register();
                 parkbench.register();
 
-                flowerbed.register();
-                denseFlowerbed.register();
+        /** garden blocks */
+        fountain.register();
+        largeplanterbox.register();
+        windowbox.register();
+        flowerbed.register();
+        denseFlowerbed.register();
 
 /*                pedestal.register();*/
+
+        /** shop blocks */
+        crateClosed.register();
+        crateApple.register();
+        crateBerry.register();
+        crateCarrot.register();
+        crateFish.register();
+        crateGrain.register();
+        crateIngot.register();
+        cratePotato.register();
+        crateSugar.register();
 
     }
 
---
+++ src/main/java/net/grallarius/sundereddeco/block/shop/BlockCrate.java
@@ -0,0 +1,11 @@
+package net.grallarius.sundereddeco.block.shop;
+
+import net.grallarius.sundereddeco.block.BlockBase;
+import net.minecraft.block.material.Material;
+
+public class BlockCrate extends BlockBase {
+
+    public BlockCrate(String name){
+        super(Material.WOOD, name);
+    }
+}
